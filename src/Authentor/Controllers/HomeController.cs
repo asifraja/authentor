bbb59ff51533c136905d7b3c41516412b5cff914
Asif Raja <Asif.Raja@easyjet.com>
2019-12-30 16:34:40 +0000

    Initial Setup

---
+++ src/Authentor/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Authentor.IdentityData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+
+            var user = await _userManager.FindByNameAsync(username);
+            if(user!=null)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            var user = new IdentityUser
+            {
+                UserName = username,
+                Email = ""
+            };
+            
+            var result = await _userManager.CreateAsync(user,password);
+
+            if(result.Succeeded)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
